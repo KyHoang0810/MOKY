70200a75b1deea4589476179d646865827659f7f
KyHoang0810 <ky.ht210505@sis.hust.edu.vn>
2025-01-20 16:10:21 +0700

    nsga time limit 50.40

--- src/main.cpp
+++ src/main.cpp
@@ -71,7 +71,7 @@ int main(int argc,char *argv[]){
     //population(populationSize);
 
     //testcount=to_string(testcountint);
-    for(int testcountint=1;testcountint<=5;testcountint++){
+    for(int testcountint=1;testcountint<=10;testcountint++){
         srand(time(nullptr));
         population=selectPopulation(popusize);
         
